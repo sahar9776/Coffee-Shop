d099e5c8ea3ff8cd1139567dc22b0f3696ca80ef
sahar9776 <sahar.samadi1376@gmail.com>
2025-01-09 12:37:30 +0330

    hooks

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+  const [value, setValue] = useState<T>(() => {
+    // value 
+    const jsonValue = localStorage.getItem(key);
+
+    // setValue
+    if (jsonValue !== null) {
+      return JSON.parse(jsonValue);
+    }
+
+    if (typeof initialValue === "function") {
+      return initialValue as () => T;
+    } else {
+      return initialValue;
+    }
+  });
+
+//   save in localStorage 
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as [typeof value, typeof setValue];
+}
